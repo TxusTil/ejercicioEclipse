0b5d395589d7231978715244635abf5e25e5e4f1
TxusTil <56879580+TxusTil@users.noreply.github.com>
2019-10-22 18:55:17 +0200

    Update Hola.java

--- src/main/Hola.java
+++ src/main/Hola.java
@@ -5,5 +5,7 @@ public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Hola");
 	        System.out.println("A todos");
+		System.out.println("¿Me has escuchado?");
+
 	}
 }	
